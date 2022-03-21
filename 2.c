2422eb4668931c9568b25d7d4896f6ccc0400c3b
Keshavgupta1234 <101824887+Keshavgupta1234@users.noreply.github.com>
2022-03-21 09:38:39 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,9 @@
+#include <conio.h>
+#include <stdio.h>
+void main()
+{
+    char *string = "456";
+    printf("Keshav Gupta \n");
+    printf("enter the value of =%900.5s", string);
+    getch();
+}
